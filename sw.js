cb5c525fa78dd5cacb7b6b92272cc0209280b313
james <51272905+banamine@users.noreply.github.com>
2025-12-14 11:47:08 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,30 @@
-self.addEventListener("install",(e=>{self.skipWaiting()}));
+const CACHE_NAME = 'liberty-radio-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    '/assets/placeholder.webp'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
